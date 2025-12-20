9869b0ea59031506e8477fdda558b0cbd6359e7f
Gabriel <gabriel_090102@hotmail.com>
2025-12-20 16:48:23 -0300

    commit 20/12/25

--- Server_Python_Interface/serial_teste.py
+++ Server_Python_Interface/serial_teste.py
@@ -13,10 +13,13 @@ CORS(app)
 CAMERA_IP = "192.168.0.157"
 USERNAME = "admin"
 PASSWORD = "Admin123"
+
 RTSP_URL = f"rtsp://{USERNAME}:{PASSWORD}@{CAMERA_IP}:554/Streaming/Channels/102"
+
 
 def generate_frames():
     cap = cv2.VideoCapture(RTSP_URL)
+
     if not cap.isOpened():
         print("❌ Erro: não foi possível conectar ao RTSP")
 
@@ -25,11 +28,14 @@ def generate_frames():
         if not success:
             continue
 
-        _, buffer = cv2.imencode('.jpg', frame)
+        _, buffer = cv2.imencode(".jpg", frame)
         frame = buffer.tobytes()
 
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        yield (
+            b"--frame\r\n"
+            b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n"
+        )
+
 
 @app.route("/camera/stream")
 def camera_stream():
@@ -37,6 +43,7 @@ def camera_stream():
         generate_frames(),
         mimetype="multipart/x-mixed-replace; boundary=frame"
     )
+
 
 # ============================================================
 # ==================== CONFIG ARDUINOS ========================
@@ -46,29 +53,31 @@ ARDUINO_LED_IP = "http://192.168.0.165/led"
 ARDUINO_FLUXO_IP = "http://192.168.0.155"
 
 color_map = {
-    'green': 'G',
-    'red': 'R',
-    'blue': 'B',
-    'yellow': 'Y',
-    'cyan': 'C',
-    'purple': 'P',
-    'ON': 'ON',
-    'OFF': 'OFF'
+    "green": "G",
+    "red": "R",
+    "blue": "B",
+    "yellow": "Y",
+    "cyan": "C",
+    "purple": "P",
+    "ON": "ON",
+    "OFF": "OFF",
 }
 
 effect_map = {
-    'Cyron': 'CYRON',
-    'Led Race': 'LED_RACE',
-    'Duo cyron': 'DUO_CYRON',
-    'RGB': 'RGB',
-    'Fade': 'FADE',
-    'Fade alternating': 'FADE_ALTERNING'
+    "Cyron": "CYRON",
+    "Led Race": "LED_RACE",
+    "Duo cyron": "DUO_CYRON",
+    "RGB": "RGB",
+    "Fade": "FADE",
+    "Fade alternating": "FADE_ALTERNING",
 }
+
 
 @app.route("/api/led", methods=["POST"])
 def control_led():
     try:
         data = request.get_json()
+
         color = data.get("color")
         effect = data.get("effect")
 
@@ -90,18 +99,21 @@ def control_led():
         print("❌ Erro LED:", e)
         return {"status": "error", "message": str(e)}, 500
 
+
 @app.route("/api/fluxo", methods=["GET"])
 def get_fluxo():
     try:
         resp = requests.get(ARDUINO_FLUXO_IP, timeout=5)
         data = resp.json()
         return jsonify(data)
+
     except Exception as e:
         print("❌ Erro lendo Arduino do fluxo:", e)
         return jsonify({
             "vazao_L_min": 0,
             "erro": str(e)
-        })
+        }), 500
+
 
 # ============================================================
 # ===================== RUN SERVER ============================
